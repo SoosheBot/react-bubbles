72460c57855e5dc5e8f593dec9b56e588094589d
sooshe <sooshe@sooshebot.com>
2020-01-31 10:43:59 -0600

    add styled-components dependency and lightly style form

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
+import './App.css';
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <header className='App-header'>
+
+        
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        </header>
       </div>
     </Router>
   );
