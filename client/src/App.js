410fe2c3fef9eee189692e40bc5c5d856d172d3f
sooshe <sooshe@sooshebot.com>
2020-01-31 12:00:34 -0600

    change log and pw for easy access

--- client/src/App.js
+++ client/src/App.js
@@ -10,31 +10,26 @@ import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
-    <div className="App">
     <Router>
-      
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="bubbles-logo" />
           <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/protected">Bubble Page</Link>
-          </li>
-        </ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/protected">Bubble Page</Link>
+            </li>
+          </ul>
         </header>
-
-        <br />
         <Switch>
-        <PrivateRoute path="/protected" component={BubblePage} />
+          <PrivateRoute path="/protected" component={BubblePage} />
           <Route path="/login" component={Login} />
           <Route component={Login} />
         </Switch>
-      
+      </div>
     </Router>
-
-    </div>
   );
 }
 
